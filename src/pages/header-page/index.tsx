d37d10574e049cf1869843d46708bc7d197229d3
Mahendran <mahendran.muthupandi@wizlogtech.com>
2024-04-28 23:47:41 +0530

    Antd Responsive Design

---
+++ src/pages/header-page/index.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Layout, Menu } from "antd";
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+// import "./header.css";
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+const HeaderPage: React.FC = () => {
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider collapsible>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Dashboard
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="Users">
+            <Menu.Item key="2">User 1</Menu.Item>
+            <Menu.Item key="3">User 2</Menu.Item>
+            <Menu.Item key="4">User 3</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Teams">
+            <Menu.Item key="5">Team 1</Menu.Item>
+            <Menu.Item key="6">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="7" icon={<DesktopOutlined />}>
+            HeaderPagelications
+          </Menu.Item>
+          <Menu.Item key="8" icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: "0 16px" }}>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360 }}
+          >
+            Content goes here
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2024 Created by You
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default HeaderPage;
